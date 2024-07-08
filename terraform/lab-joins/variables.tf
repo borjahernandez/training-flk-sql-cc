317fcb5dd506c6dc0730a64c4b8861a9e6f2364a
borjahernandez <borja.hernandez.crespo@gmail.com>
2024-07-08 23:34:43 +0200

    adding lab-joins folder

---
+++ terraform/lab-joins/variables.tf
@@ -0,0 +1,21 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+  type        = string
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "confluent_cloud_provider" {
+  description = "Cloud Provider (AWS, GCP, Azure)"
+  type        = string
+}
+
+variable "confluent_cloud_region" {
+  description = "Cloud region ID based on the provider"
+  type        = string
+  sensitive   = true
+}
